6361522096c13c139101a80a8782149d9f3199bd
unknown <luiscf1226@gmail.com>
2020-10-30 17:00:32 -0600

    Modificacion

--- Lab3_LuisFlores_JesusMeraz/src/lab3_luisflores_jesusmeraz/Lab3_LuisFlores_JesusMeraz.java
+++ Lab3_LuisFlores_JesusMeraz/src/lab3_luisflores_jesusmeraz/Lab3_LuisFlores_JesusMeraz.java
@@ -98,7 +98,7 @@ public class Lab3_LuisFlores_JesusMeraz {
                                         System.out.println("2. Crear Clientes");
                                         System.out.println("3. Salir");
                                         System.out.println("Escoja Opcion: ");
-                                        o2= leer.nextInt();
+                                        o2 = leer.nextInt();
                                         switch (o2) {
                                             case 1:
                                                 CrearEmp();
@@ -137,7 +137,7 @@ public class Lab3_LuisFlores_JesusMeraz {
 
                                     break;
                                 case 4:
-                                  
+
                                     System.out.println("LOCALES!!!!");
                                     String salida = "";
                                     for (Object t : locales) {
@@ -147,26 +147,102 @@ public class Lab3_LuisFlores_JesusMeraz {
                                     break;
                                 case 5:
                                     System.out.println("PERSONAS!!!!");
-                                    String salida2= "";
+                                    String salida2 = "";
                                     for (Object t : personas) {
                                         salida2 += "" + personas.indexOf(t) + "- " + t + "\n";
                                     }
                                     System.out.println(salida2);
-                                    
-                                break;
+
+                                    break;
                                 case 6:
                                     System.out.println("PRODUCTOS!!!!");
-                                    String salida3= "";
+                                    String salida3 = "";
                                     for (Object t : productos) {
                                         salida3 += "" + productos.indexOf(t) + "- " + t + "\n";
                                     }
                                     System.out.println(salida3);
-                                break;
+                                    break;
+                                case 7:
+                                    int o7 = 0;
+                                    while (o7 != 4) {
+                                        System.out.println("Modificando Local");
+                                        System.out.println("1. Tienda");
+                                        System.out.println("2. Quisco");
+                                        System.out.println("3. Restaurante");
+                                        System.out.println("4. Salir");
+                                        System.out.println("Escoja Opcion: ");
+                                        o7 = leer.nextInt();
+                                        switch (o7) {
+                                            case 1:
+                                                Modtienda();
+
+                                                break;
+                                            case 2:
+                                                ModQ();
+
+                                                break;
+                                            case 3:
+                                                ModR();
+
+                                                break;
+                                        }
+                                    }
+                                    break;
+                                case 8:
+                                    int o8 = 0;
+                                    while (o8 != 3) {
+                                        System.out.println("Modificar Persona");
+                                        System.out.println("1. Empleados");
+                                        System.out.println("2.  Clientes");
+                                        System.out.println("3. Salir");
+                                        System.out.println("Escoja Opcion: ");
+                                        o8 = leer.nextInt();
+                                        switch (o8) {
+                                            case 1:
+                                                Mode();
+
+                                                break;
+                                            case 2:
+                                                Modc();
+                                                break;
+
+                                        }
+                                    }
+                                    break;
+                                case 9:
+                                    int o9 = 0;
+                                    while (o9 != 4) {
+                                        System.out.println("Modificando Producto");
+                                        System.out.println("1. Comida");
+                                        System.out.println("2. Juguete");
+                                        System.out.println("3. Ropa");
+                                        System.out.println("4. Salir");
+                                        System.out.println("Escoja Opcion: ");
+                                        o9 = leer.nextInt();
+                                        switch (o9) {
+                                            case 1:
+                                                ModComida();
+
+                                                break;
+                                            case 2:
+
+                                                break;
+                                            case 3:
+
+                                                break;
+
+                                        }
+                                    }
+                                    break;
                             }
 
                         }
                     }
-            }
+                case 2:
+                    System.out.println("Bienvenido Usuario");
+                    break;
+            }// LOGIN JESUS USUARIO
+
         }
     }
 
@@ -421,5 +497,264 @@ public class Lab3_LuisFlores_JesusMeraz {
         Comida c = new Comida(f, t, d, n, p);
         productos.add(c);
         comida.add(c);
+    }
+
+    public static void Modtienda() {
+        //public void  Tiendas(Ropa ropa, Juguetes juguetes) {
+        if (tiendas.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= tiendas.size());
+        System.out.println(" Ingrese tipo de ropa: ");
+        String tipo = leer.next();
+        System.out.println("Ingrese su talla: ");
+        String talla = leer.next();
+        System.out.println("Ingrese Descripcion: ");
+        String descripcion = leer.next();
+        System.out.println("Nombre Ropa: ");
+        String nombre_ropa = leer.next();
+        System.out.println("Ingrese Precio: ");
+        int precio = leer.nextInt();
+        Ropa ropa1 = new Ropa(tipo, talla, descripcion, nombre_ropa, precio);//OBJETO ROPA
+        System.out.println("Ingrese Descripcion Producto: ");
+        String desc_prod = leer.next();
+        System.out.println("Ingrese Descripcion Juguete: ");
+        String descripcion2 = leer.next();
+        System.out.println("Ingrese Nombre Juguete: ");
+        String name_jug = leer.next();
+        System.out.println("Ingrese Precio: ");
+        int precio_jug = leer.nextInt();
+        Juguetes jug1 = new Juguetes(desc_prod, descripcion2, name_jug, precio_jug);//OBJETO JUGUETE
+        tiendas.get(indice).Act(nombre_ropa, name_jug);
+        System.out.println("NUEVO");
+        System.out.println(tiendas);
+    }
+
+    public static void ModQ() {
+        //public void Quisco2(String nombre_tienda, Ropa ropa, Juguetes juguetes) 
+        if (quisco.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= quisco.size());
+        System.out.println(" Ingrese tipo de ropa: ");
+        String tipo = leer.next();
+        System.out.println("Ingrese su talla: ");
+        String talla = leer.next();
+        System.out.println("Ingrese Descripcion: ");
+        String descripcion = leer.next();
+        System.out.println("Nombre Ropa: ");
+        String nombre_ropa = leer.next();
+        System.out.println("Ingrese Precio: ");
+        int precio = leer.nextInt();
+        Ropa ropa1 = new Ropa(tipo, talla, descripcion, nombre_ropa, precio);//OBJETO ROPA
+        System.out.println("Ingrese Descripcion Producto: ");
+        String desc_prod = leer.next();
+        System.out.println("Ingrese Descripcion Juguete: ");
+        String descripcion2 = leer.next();
+        System.out.println("Ingrese Nombre Juguete: ");
+        String name_jug = leer.next();
+        System.out.println("Ingrese Precio: ");
+        int precio_jug = leer.nextInt();
+        Juguetes jug1 = new Juguetes(desc_prod, descripcion2, name_jug, precio_jug);//OBJETO JUGUETE
+        System.out.println("Nombre Tienda: ");
+        String n11 = leer.next();
+        quisco.get(indice).Quisco2(n11, ropa1, jug1);
+        System.out.println("Nuevo");
+        System.out.println(quisco);
+    }
+
+    public static void ModR() {
+        // public void  act(Empleados empleado1, Empleados empleado2, Empleados empleado3, Empleados empleado4
+        if (restaurantes.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= restaurantes.size());
+        System.out.println("Horario 1: ");
+        String h1 = leer.next();
+        System.out.println("Productos Empleados");
+        int p1 = leer.nextInt();
+        System.out.println("ID empleado: ");
+        int id1 = leer.nextInt();
+        System.out.println("User Empleado 1: ");
+        String u1 = leer.next();
+        System.out.println("Contra Empleado1: ");
+        String c1 = leer.next();
+        System.out.println("Correo empleado 1: ");
+        String correo1 = leer.next();
+        System.out.println("Nombre Empleado 1: ");
+        String n1 = leer.next();
+        Empleados e1 = new Empleados(h1, p1, id1, u1, c1, correo1, n1);//EMPLEADO 1
+        System.out.println("Horario 2: ");
+        String h2 = leer.next();
+        System.out.println("Productos Empleados");
+        int p2 = leer.nextInt();
+        System.out.println("ID empleado: ");
+        int id2 = leer.nextInt();
+        System.out.println("User Empleado 2: ");
+        String u2 = leer.next();
+        System.out.println("Contra Empleado 2: ");
+        String c2 = leer.next();
+        System.out.println("Correo empleado 2: ");
+        String correo2 = leer.next();
+        System.out.println("Nombre Empleado 2: ");
+        String n2 = leer.next();
+        Empleados e2 = new Empleados(h2, p2, id2, u2, c1, correo2, n2);//EMPLEADO 2
+        System.out.println("Horario 3: ");
+        String h3 = leer.next();
+        System.out.println("Productos Empleados");
+        int p3 = leer.nextInt();
+        System.out.println("ID empleado: ");
+        int id3 = leer.nextInt();
+        System.out.println("User Empleado 3: ");
+        String u3 = leer.next();
+        System.out.println("Contra Empleado 3: ");
+        String c3 = leer.next();
+        System.out.println("Correo empleado 3: ");
+        String correo3 = leer.next();
+        System.out.println("Nombre Empleado 3: ");
+        String n3 = leer.next();
+        Empleados e3 = new Empleados(h3, p3, id3, u3, c3, correo3, n3);//EMPLEADO 3
+        System.out.println("Horario 4: ");
+        String h4 = leer.next();
+        System.out.println("Productos Empleados");
+        int p4 = leer.nextInt();
+        System.out.println("ID empleado: ");
+        int id4 = leer.nextInt();
+        System.out.println("User Empleado 4: ");
+        String u4 = leer.next();
+        System.out.println("Contra Empleado 4: ");
+        String c4 = leer.next();
+        System.out.println("Correo empleado 4: ");
+        String correo4 = leer.next();
+        System.out.println("Nombre Empleado 4: ");
+        String n4 = leer.next();
+        Empleados e4 = new Empleados(h4, p4, id4, u4, c4, correo4, n4);//EMPLEADO 4
+        restaurantes.get(indice).act(e1, e2, e3, e4);
+        System.out.println("NUEVO");
+        System.out.println(restaurantes);
+
+    }
+
+    public static void Mode() {
+        // public void  actEmpleados(String horario, int productos) {
+        if (empleados.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= empleados.size());
+        System.out.println("Ingrese Nuevo Horario: ");
+        String h = leer.next();
+        System.out.println("Nueva cantidad productos: ");
+        int pp = leer.nextInt();
+        empleados.get(indice).actEmpleados(h, pp);
+        System.out.println("NUEVO");
+        System.out.println(empleados);
+    }
+
+    public static void Modc() {
+        //public void act(double dinero) {
+        if (clientes.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= clientes.size());
+        System.out.println("Ingrese Nueva Cantidad Dinero: ");
+        int cantidad = leer.nextInt();
+        clientes.get(indice).act(indice);
+        System.out.println("Nuevo: ");
+        System.out.println(clientes);
+
+    }
+
+    public static void ModComida() {
+        if (comida.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= comida.size());
+        // public void Comida2(String fecha, String tipo_productos) {
+        System.out.println("Ingrese Nueva fecha: ");
+        String f = leer.next();
+        System.out.println("Ingrese nuevo Tipo: ");
+        String t = leer.next();
+        comida.get(indice).Comida2(f, t);
+        System.out.println("Nuevo");
+        System.out.println(comida);
+    }
+
+    public static void ModJuguete() {
+        if (juguetes.isEmpty()) {
+            System.out.println("No hay jugadores");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("MODIFICAR!");
+
+        int indice;
+
+        do {
+            System.out.println("->Ingrese posicion a modificar: ");
+            indice = sc.nextInt();
+        } while (indice < 0 || indice >= juguetes.size());
+        System.out.println("Ingrese nuva descripcion: ");
+        String c = leer.next();
     }
 }
